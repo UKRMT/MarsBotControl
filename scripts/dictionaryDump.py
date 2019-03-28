3eea94595ace6ce31ad4f07bb5ae383474e5d3e7
Matt Ruffner <mattruffner7@gmail.com>
2019-03-28 23:15:04 +0000

    working multi-client server with shared state and separate ping script

--- scripts/dictionaryDump.py
+++ scripts/dictionaryDump.py
@@ -1,8 +1,11 @@
 import json
 
-dictionaryFile = '/var/www/html/web-control/src/plugins/dictionary-plugin/dictionary.json'
+def getDictionaryKeys():
+    dictionaryFile = '/var/www/html/web-control/src/plugins/dictionary-plugin/dictionary.json'
+    dictionary = {}
 
-with open(dictionaryFile) as json_file:
-    data = json.load(json_file)
-    for p in data['measurements']:
-        print(p['name'] + ':\t\t' + p['key'])
+    with open(dictionaryFile) as json_file:
+        data = json.load(json_file)
+        for p in data['measurements']:
+            dictionary[p['key']]=0
+    return dictionary
