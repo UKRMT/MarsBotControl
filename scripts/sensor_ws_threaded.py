129cc23bc863a120066378fd94abf72030f55857
Matt Ruffner <mattruffner7@gmail.com>
2019-03-29 19:16:00 +0000

    thread lib edits, problem starting thread with async def

--- scripts/sensor_ws_threaded.py
+++ scripts/sensor_ws_threaded.py
@@ -13,55 +13,59 @@ Revision History:
 import asyncio
 import websockets
 import json
+import datetime
 import time
-import thread
+import _thread
 from dictionaryDump import getDictionaryKeys as gdk
+
 class SensorsWebSocket():
     thread_dict = {}
-    sensor_dict =  {}
+    sensor_dict = {}
     '''
     Method: initialize
     Description: initalizes the sensor poster by setting up the dictionary json as an instance accessable array.
     '''
-    def __init__():
+    def __init__(self):
         self.sensor_dict = gdk()
-        start_server = websocket.serve(listener, 'localhost', 8765)
+        thread_dict = {}
+        start_server = websockets.serve(self.listener, port=1234)
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
     '''
     Method: listener
     Description: Monitors the socket for a message from openMCT and checks what data the openMCT wants against the dictionary in the instance.
     '''
-    async def listener(websocket, path):
+    async def listener(self, websocket, path):
         try:
-        while True:
-            sensor_request = await websocket.recv()
-            sensor_array = sensor_request.split(" ")
-
-            if sensor_array[0] == 'unsubscribe':
-                #check what mct wants to unsubscribe to and kill that thread
-                if self.thread_dict[sensor_array[1]]:
-                    self.thread_dict[sensor_array[1]].exit()
-                    self.thread_dict.pop(sensor_array[1])
-            elif sensor_array[1] == 'subscribe':
-                #add a thread based on what open mct wants to subscribe to
-                new_thread = thread.start_new_thread(sensor_blast, (websocket, sensor_array[1]))
-                self.thread_dict[sensor_array[1]] = new_thread
-            else:
-                print('Looking for either subscribe or unsubscribe, neither was given')
+            while True:
+                sensor_request = await websocket.recv()
+                print('got request: ' + sensor_request)
+                sensor_array = sensor_request.split(" ")
+                
+                if sensor_array[0] == 'unsubscribe':
+                    #check what mct wants to unsubscribe to and kill that thread
+                    if self.thread_dict[sensor_array[1]]:
+                        self.thread_dict[sensor_array[1]].exit()
+                        self.thread_dict.pop(sensor_array[1])
+                elif sensor_array[0] == 'subscribe':
+                        #add a thread based on what open mct wants to subscribe to
+                        new_thread = _thread.start_new_thread(self.sensor_blast, (websocket, sensor_array[1]))
+                        self.thread_dict[sensor_array[1]] = new_thread
+                else:
+                    print('Looking for either subscribe or unsubscribe, neither was given')
         except websockets.ConnectionClosed:
-            for key, thread in self.thread_dict.iterItems():
+            for key, thread in self.thread_dict.iteritems():
                 thread.exit()
                 self.thread_dict.pop(key)
-            print('Connection Closed, threads killed')
-        except e:
-            print('Unexpected Error')
-            print(e)
+                print('Connection Closed, threads killed')
+#        except e:
+#            print('Unexpected Error')
+#            print(e)
     '''
     Method: sensor_blast
     Description: Blasts sensor data through the websocket in a readable format by MCT, this will be a thread automatically started and killed
     '''
-    async def sensor_blast(websocket, sensor_type):
+    def sensor_blast(self, websocket, sensor_type):
         while True:
             try:
                 # get sensor data base on sensor type
@@ -69,12 +73,15 @@ class SensorsWebSocket():
                 # push it through the socket connection
                 ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
                 time_num = time.time()
-                message = json.dumps({'id':sensor_type,'timestamp':ts,'value':sensor_dict[sensor_type]})
-                await websocket.send(message)
+                message = json.dumps({'id':sensor_type,'timestamp':ts,'value':self.sensor_dict[sensor_type]})
+                print('sending' +  message)
+                websocket.send(message)
                 time.sleep(1)
             except:
                 print("unexpected sensor key")
-                print(sensor_dict)
+                print(self.sensor_dict)
                 print(sensor_type)
 
 
+                
+s = SensorsWebSocket()
